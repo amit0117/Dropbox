2124e57dc634be029aaf2befe9f0456c08ee5751
amit0117 <amitdnagar4@gmail.com>
2026-02-16 00:48:55 +0530

    Frontend Project setup and layout

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { ColorSchemeScript, MantineProvider, createTheme } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Sample Dropbox",
+  description: "Upload and manage your files",
+};
+
+const theme = createTheme({});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <ColorSchemeScript defaultColorScheme="dark" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <MantineProvider defaultColorScheme="dark" theme={theme}>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
