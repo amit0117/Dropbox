deb5ef8ddd4d5e127dff5ed0c7dbf377df122871
amit0117 <amitdnagar4@gmail.com>
2026-02-16 01:17:10 +0530

    Updated deployed fe for cors

--- backend/app/constants/constants.py
+++ backend/app/constants/constants.py
@@ -8,4 +8,4 @@ DEFAULT_PAGE_SKIP: int = 0
 DEFAULT_PAGE_LIMIT: int = 20
 MAX_PAGE_LIMIT: int = 100
 
-ALLOWED_ORIGINS: list[str] = ["http://localhost:3000", "http://127.0.0.1:3000"]
+ALLOWED_ORIGINS: list[str] = ["http://localhost:3000", "http://127.0.0.1:3000", "https://dropbox-seven-steel.vercel.app"]
